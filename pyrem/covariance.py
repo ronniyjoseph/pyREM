aa67db7fe2f161591a60361a1c059984acdb5176
Ronniy Joseph <ronniy.joseph@icrar.org>
2022-02-03 11:02:46 -0500

    Renamed everything from numpy to np

--- pyrem/covariance.py
+++ pyrem/covariance.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 from scipy.constants import c
 from scipy import signal
 
@@ -7,14 +7,108 @@ from .radiotelescope import mwa_dipole_locations
 
 from .skymodel import sky_moment_returner
 from .powerspectrum import compute_power
-from matplotlib import pyplot
+from matplotlib import pyplot as plt
+
+
+class CovarianceMatrix:
+
+    def __init__(self, u, v, nu):
+        self.u = u
+        self.v = v
+        self.nu = nu
+
+        self.beam_model =  None
+        self.baselines =  None
+        self.matrix =  None
+        self.eigenvalues = None
+        self.eigenmodes =  None
+
+        return
+
+    def gaussian(self, aperture_diameter=2, S_low=1e-3, S_mid=1, S_high=10, gamma=0.8, nu_0 = 150e6):
+        """
+        Flat-sky gaussian approximation of a antenna beam
+
+        """
+        #Compute 2nd moment of the Point Source counts function thingy
+
+        #So this point source sky model is calibrated for 150 MHz, can we scale this?
+        u, v, nu =  self.return_grid()
+        mu = sky_moment_returner(2, s_low=S_low, s_mid=S_mid, s_high=S_high)
+
+        #If we're computing the covariance matrix over multiple frequencies we still want to end up with a 2D array that
+        #that has the baseline-frequency covariance
+
+        beam_width1 = beam_width(nu[0], diameter=aperture_diameter)
+        beam_width2 = beam_width(nu[1], diameter=aperture_diameter)
+        sigma_nu = beam_width1 ** 2 * beam_width2 ** 2 / (beam_width1 ** 2 + beam_width2 ** 2)
+
+        kernel = -2 * np.pi ** 2 * sigma_nu * ( (u[0] * nu[0] - u[1] * nu[1]) ** 2 +
+                                                (v[0] * nu[0] - v[1] * nu[1]) ** 2) / nu_0 ** 2
+        covariance = 2 * np.pi * mu * sigma_nu * (nu[0] * nu[1] / nu_0 ** 2) ** (- gamma) * np.exp(kernel)
+
+        plt.imshow(covariance)
+        plt.show()
+        return
+
+    def return_grid(self):
+        uu1, uu2 = np.meshgrid(self.u, self.u)
+        vv1, vv2 = np.meshgrid(self.v, self.v)
+
+        u_grid1 = np.tile(uu1, np.array([len(self.nu), len(self.nu)]))
+        u_grid2 = np.tile(uu2, np.array([len(self.nu), len(self.nu)]))
+
+        v_grid1 = np.tile(vv1, np.array([len(self.nu), len(self.nu)]))
+        v_grid2 = np.tile(vv2, np.array([len(self.nu), len(self.nu)]))
+
+        nu_grid1 = np.zeros_like(u_grid1)
+        nu_grid2 = np.zeros_like(u_grid1)
+
+        # Create the full covariance_coordinate grid?
+        for i in range(len(self.nu)):
+            nu_grid1[i * len(self.u):(i + 1) * len(self.u), :] = self.nu[i]
+            nu_grid2[:, i * len(self.u):(i + 1) * len(self.u)] = self.nu[i]
+
+        return (u_grid1, u_grid2), (v_grid1, v_grid2), (nu_grid1, nu_grid2)
+
+
+
+    def airy(self):
+        """
+        Flat-sky airy beam approximation of an antenna beam
+        """
+        return
+
+    def actualbeam(self):
+        return
+
+    def compute(self):
+        return
+
+    def decompose(self):
+        return
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 def position_covariance(u, v, nu, position_precision = 1e-2, gamma = 0.8, mode = "frequency", nu_0 = 150e6,
                         tile_diameter = 4, s_high = 10):
     mu_1 = sky_moment_returner(n_order = 1, s_high= s_high)
     mu_2 = sky_moment_returner(n_order = 2, s_high= s_high)
     if mode == "frequency":
-        nn1, nn2 = numpy.meshgrid(nu, nu)
+        nn1, nn2 = np.meshgrid(nu, nu)
         vv1 = v
         vv2 = v
         uu1 = u
@@ -23,8 +117,8 @@ def position_covariance(u, v, nu, position_precision = 1e-2, gamma = 0.8, mode =
     else:
         nn1 = nu
         nn2 = nu
-        vv1, vv2 = numpy.meshgrid(v, v)
-        uu1, uu2 = numpy.meshgrid(u, u)
+        vv1, vv2 = np.meshgrid(v, v)
+        uu1, uu2 = np.meshgrid(u, u)
         delta_u = position_precision*nu / c
 
     beamwidth1 = beam_width(nn1, diameter=tile_diameter)
@@ -32,8 +126,8 @@ def position_covariance(u, v, nu, position_precision = 1e-2, gamma = 0.8, mode =
 
     sigma = beamwidth1**2*beamwidth2**2/(beamwidth1**2 + beamwidth2**2)
 
-    kernel = -2*numpy.pi**2*sigma*((uu1*nn1 - uu2*nn2)**2 + (vv1*nn1 - vv2*nn2)**2 )/nu_0**2
-    a = 16*numpy.pi**3*mu_2*(nn1*nn2/nu_0**2)**(1-gamma)*delta_u**2*sigma*numpy.exp(kernel)*(1+2*kernel)
+    kernel = -2*np.pi**2*sigma*((uu1*nn1 - uu2*nn2)**2 + (vv1*nn1 - vv2*nn2)**2 )/nu_0**2
+    a = 16*np.pi**3*mu_2*(nn1*nn2/nu_0**2)**(1-gamma)*delta_u**2*sigma*np.exp(kernel)*(1+2*kernel)
     b = mu_1**2*(nn1*nn2)**(-gamma)*delta_u**2
 
     covariance = a
@@ -52,8 +146,8 @@ def beam_covariance(u, v, nu, dx = 1.1, gamma= 0.8, mode = 'frequency', broken_t
     mu_2_m = sky_moment_returner(2, s_low=model_limit, s_high=10)
 
     if mode == "frequency":
-        nn1, nn2, xx = numpy.meshgrid(nu, nu, x_offsets)
-        nn1, nn2, yy = numpy.meshgrid(nu, nu, y_offsets)
+        nn1, nn2, xx = np.meshgrid(nu, nu, x_offsets)
+        nn1, nn2, yy = np.meshgrid(nu, nu, y_offsets)
         vv1 = v
         vv2 = v
         uu1 = u
@@ -62,16 +156,16 @@ def beam_covariance(u, v, nu, dx = 1.1, gamma= 0.8, mode = 'frequency', broken_t
     else:
         nn1 = nu
         nn2 = nu
-        vv1, vv2, yy = numpy.meshgrid(v, v, y_offsets)
-        uu1, uu2, xx = numpy.meshgrid(u, u, x_offsets)
+        vv1, vv2, yy = np.meshgrid(v, v, y_offsets)
+        uu1, uu2, xx = np.meshgrid(u, u, x_offsets)
         frequency_scaling = nu**2/nu_0**2
 
-    width_1_tile = numpy.sqrt(2) * beam_width(frequency=nn1, diameter=tile_diameter)
-    width_2_tile = numpy.sqrt(2) * beam_width(frequency=nn2, diameter=tile_diameter)
-    width_1_dipole = numpy.sqrt(2) * beam_width(frequency=nn1, diameter=1)
-    width_2_dipole = numpy.sqrt(2) * beam_width(frequency=nn2, diameter=1)
+    width_1_tile = np.sqrt(2) * beam_width(frequency=nn1, diameter=tile_diameter)
+    width_2_tile = np.sqrt(2) * beam_width(frequency=nn2, diameter=tile_diameter)
+    width_1_dipole = np.sqrt(2) * beam_width(frequency=nn1, diameter=1)
+    width_2_dipole = np.sqrt(2) * beam_width(frequency=nn2, diameter=1)
 
-    kernel = -2 * numpy.pi ** 2 * ((uu1*nn1 - uu2*nn2 + xx*(nn1 - nn2) / c) ** 2 +
+    kernel = -2 * np.pi ** 2 * ((uu1*nn1 - uu2*nn2 + xx*(nn1 - nn2) / c) ** 2 +
                                              (vv1*nn1 - vv2*nn2 + yy*(nn1 - nn2) / c) ** 2)/nu_0**2
 
     sigma_a = (width_1_tile * width_2_tile * width_1_dipole * width_2_dipole) ** 2 / (
@@ -91,22 +185,22 @@ def beam_covariance(u, v, nu, dx = 1.1, gamma= 0.8, mode = 'frequency', broken_t
     sigma_d1 = width_1_tile ** 2 * width_1_dipole ** 2 / (width_1_tile ** 2 + width_1_dipole ** 2)
     sigma_d2 = width_2_tile ** 2 * width_2_dipole ** 2 / (width_2_tile ** 2 + width_2_dipole ** 2)
 
-    covariance_a = 2 * numpy.pi *frequency_scaling**(-gamma)* (mu_2_m + mu_2_r) /len(y_offsets) ** 3 * \
-        numpy.sum(sigma_a * numpy.exp(sigma_a*kernel), axis=-1)
+    covariance_a = 2 * np.pi *frequency_scaling**(-gamma)* (mu_2_m + mu_2_r) /len(y_offsets) ** 3 * \
+        np.sum(sigma_a * np.exp(sigma_a*kernel), axis=-1)
 
-    covariance_b = -2 * numpy.pi *frequency_scaling**(-gamma)* mu_2_r / len(y_offsets) ** 2 * \
-        numpy.sum(sigma_b * numpy.exp(sigma_b*kernel), axis=-1)
+    covariance_b = -2 * np.pi *frequency_scaling**(-gamma)* mu_2_r / len(y_offsets) ** 2 * \
+        np.sum(sigma_b * np.exp(sigma_b*kernel), axis=-1)
 
-    covariance_c = -2 * numpy.pi *frequency_scaling**(-gamma)* mu_2_r / len(y_offsets) ** 2 * \
-        numpy.sum(sigma_c * numpy.exp(sigma_c *kernel),axis=-1)
+    covariance_c = -2 * np.pi *frequency_scaling**(-gamma)* mu_2_r / len(y_offsets) ** 2 * \
+        np.sum(sigma_c * np.exp(sigma_c *kernel),axis=-1)
 
-    covariance_d = 2 * numpy.pi *frequency_scaling**(-gamma)* (mu_1_m + mu_1_r)**2 * \
-        numpy.sum( sigma_d1 * sigma_d2 / len(x_offsets) ** 3 * \
-        numpy.exp(sigma_d1 * kernel)* numpy.exp(sigma_d2 * kernel), axis=-1)
+    covariance_d = 2 * np.pi *frequency_scaling**(-gamma)* (mu_1_m + mu_1_r)**2 * \
+        np.sum( sigma_d1 * sigma_d2 / len(x_offsets) ** 3 * \
+        np.exp(sigma_d1 * kernel)* np.exp(sigma_d2 * kernel), axis=-1)
 
-    covariance_e = -2 * numpy.pi *frequency_scaling**(-gamma)*(mu_1_m + mu_1_r)**2 * \
-        numpy.sum(sigma_d1 * sigma_d2 / len(x_offsets) ** 4 * numpy.exp(sigma_d1 * kernel), axis=-1) * \
-        numpy.sum(numpy.exp(sigma_d2 * kernel), axis=-1)
+    covariance_e = -2 * np.pi *frequency_scaling**(-gamma)*(mu_1_m + mu_1_r)**2 * \
+        np.sum(sigma_d1 * sigma_d2 / len(x_offsets) ** 4 * np.exp(sigma_d1 * kernel), axis=-1) * \
+        np.sum(np.exp(sigma_d2 * kernel), axis=-1)
 
     if calibration_type == "redundant":
         covariance = broken_tile_fraction**2*(covariance_a + covariance_d + covariance_e)
@@ -121,7 +215,7 @@ def sky_covariance(u, v, nu, S_low=1e-3, S_mid=1, S_high=10, gamma=0.8, mode = '
 
     mu_2 = sky_moment_returner(2, s_low=S_low, s_mid=S_mid, s_high=S_high)
     if mode == "frequency":
-        nn1, nn2 = numpy.meshgrid(nu, nu)
+        nn1, nn2 = np.meshgrid(nu, nu)
         uu1 = u
         uu2 = u
         vv1 = v
@@ -129,22 +223,22 @@ def sky_covariance(u, v, nu, S_low=1e-3, S_mid=1, S_high=10, gamma=0.8, mode = '
     else:
         nn1 = nu
         nn2 = nu
-        uu1, uu2 = numpy.meshgrid(u, u)
-        vv1, vv2 = numpy.meshgrid(v, v)
+        uu1, uu2 = np.meshgrid(u, u)
+        vv1, vv2 = np.meshgrid(v, v)
 
     width_tile1 = beam_width(nn1, diameter=tile_diameter)
     width_tile2 = beam_width(nn2, diameter=tile_diameter)
     sigma_nu = width_tile1**2*width_tile2**2/(width_tile1**2 + width_tile2**2)
 
 
-    kernel = -2*numpy.pi ** 2 * sigma_nu * ((uu1*nn1 - uu2*nn2) ** 2 + (vv1*nn1 - vv2*nn2) ** 2)/nu_0**2
-    covariance = 2 * numpy.pi * mu_2 * sigma_nu * (nn1*nn2/nu_0**2)**(-gamma)*numpy.exp(kernel)
+    kernel = -2*np.pi ** 2 * sigma_nu * ((uu1*nn1 - uu2*nn2) ** 2 + (vv1*nn1 - vv2*nn2) ** 2)/nu_0**2
+    covariance = 2 * np.pi * mu_2 * sigma_nu * (nn1*nn2/nu_0**2)**(-gamma)*np.exp(kernel)
 
     return covariance
 
 
 def thermal_variance(sefd=20e3, bandwidth=40e3, t_integrate=120):
-    variance = (sefd / numpy.sqrt(bandwidth * t_integrate))**2
+    variance = (sefd / np.sqrt(bandwidth * t_integrate))**2
 
     return variance
 
@@ -155,16 +249,16 @@ def gain_error_covariance(u_range, frequency_range, residuals='both', weights=No
                           model_limit=1):
 
     if calibration_type == "sky":
-        model_variance = numpy.diag(sky_covariance(0, 0, frequency_range, S_low=model_limit, S_high=10,
+        model_variance = np.diag(sky_covariance(0, 0, frequency_range, S_low=model_limit, S_high=10,
                                                    tile_diameter=tile_diameter))
     elif calibration_type == 'relative':
-        model_variance = numpy.diag(sky_covariance(0, 0, frequency_range, S_low=1e-3, S_high=10,
+        model_variance = np.diag(sky_covariance(0, 0, frequency_range, S_low=1e-3, S_high=10,
                                                    tile_diameter=tile_diameter))
 
-    model_normalisation = numpy.sqrt(numpy.outer(model_variance, model_variance))
+    model_normalisation = np.sqrt(np.outer(model_variance, model_variance))
 
 
-    covariance = numpy.zeros((len(u_range), len(frequency_range), len(frequency_range)))
+    covariance = np.zeros((len(u_range), len(frequency_range), len(frequency_range)))
 
     # Compute all residual to model ratios at different u scales
     for u_index in range(len(u_range)):
@@ -207,25 +301,25 @@ def gain_error_covariance(u_range, frequency_range, residuals='both', weights=No
         covariance[u_index, :, :] = residual_covariance / model_normalisation
 
     if weights is None:
-        gain_averaged_covariance = numpy.sum(covariance, axis=0) * (1/((N_antenna - 1)*len(u_range))) ** 2
+        gain_averaged_covariance = np.sum(covariance, axis=0) * (1/((N_antenna - 1)*len(u_range))) ** 2
     else:
         gain_averaged_covariance = covariance.copy()
         for u_index in range(len(u_range)):
-            u_weight_reshaped = numpy.tile(weights[u_index, :].flatten(), (len(frequency_range), len(frequency_range), 1)).T
-            gain_averaged_covariance[u_index, ...] = numpy.sum(covariance * u_weight_reshaped, axis=0)
+            u_weight_reshaped = np.tile(weights[u_index, :].flatten(), (len(frequency_range), len(frequency_range), 1)).T
+            gain_averaged_covariance[u_index, ...] = np.sum(covariance * u_weight_reshaped, axis=0)
 
     return gain_averaged_covariance
 
 
 def compute_weights(u_cells, u, v, N_antenna = 128):
-    u_bin_edges = numpy.zeros(len(u_cells) + 1)
-    baseline_lengths = numpy.sqrt(u**2 + v**2)
-    log_steps = numpy.diff(numpy.log10(u_cells))
-    u_bin_edges[1:] = 10**(numpy.log10(u_cells) + 0.5*log_steps[0])
-    u_bin_edges[0] = 10**(numpy.log10(u_cells[0] - 0.5*log_steps[0]))
-    counts, bin_edges = numpy.histogram(baseline_lengths, bins=u_bin_edges)
+    u_bin_edges = np.zeros(len(u_cells) + 1)
+    baseline_lengths = np.sqrt(u**2 + v**2)
+    log_steps = np.diff(np.log10(u_cells))
+    u_bin_edges[1:] = 10**(np.log10(u_cells) + 0.5*log_steps[0])
+    u_bin_edges[0] = 10**(np.log10(u_cells[0] - 0.5*log_steps[0]))
+    counts, bin_edges = np.histogram(baseline_lengths, bins=u_bin_edges)
 
-    prime, unprime = numpy.meshgrid(counts / len(baseline_lengths), counts / len(baseline_lengths))
+    prime, unprime = np.meshgrid(counts / len(baseline_lengths), counts / len(baseline_lengths))
     weights = prime * unprime * (2 / (N_antenna - 1)) ** 2
     pyplot.imshow(weights.T, origin='lower')
     pyplot.show()
@@ -237,15 +331,15 @@ def calculcate_absolute_calibration(sky_averaged_covariance, weights):
         absolute_averaged_covariance = sky_averaged_covariance
     else:
 
-        absolute_averaged_covariance = numpy.zeros_like(sky_averaged_covariance)
+        absolute_averaged_covariance = np.zeros_like(sky_averaged_covariance)
         for i in range(sky_averaged_covariance.shape[0]):
-            absolute_averaged_covariance[i, ...] = numpy.mean(sky_averaged_covariance, axis=0)
+            absolute_averaged_covariance[i, ...] = np.mean(sky_averaged_covariance, axis=0)
 
     return absolute_averaged_covariance
 
 
 # def uncalibrated_residual_error():
-#     raw_variance = numpy.zeros((len(u), int(len(nu) / 2)))
+#     raw_variance = np.zeros((len(u), int(len(nu) / 2)))
 #
 #     for i in range(len(u)):
 #         if calibration_type == "redundant":
@@ -285,7 +379,7 @@ def calculcate_absolute_calibration(sky_averaged_covariance, weights):
 def calibrated_residual_error(u, nu, residuals='both', broken_baselines_weight = 1, weights = None,
                              calibration_type = 'sky', scale_limit = None, N_antenna = 128, tile_diameter=4,
                               position_error = 0.02, model_limit=1):
-    cal_variance = numpy.zeros((len(u), int(len(nu) / 2)))
+    cal_variance = np.zeros((len(u), int(len(nu) / 2)))
 
     if calibration_type == "sky":
         gain_averaged_covariance = gain_error_covariance(u, nu, residuals=residuals, calibration_type=calibration_type,
@@ -326,7 +420,7 @@ def calibrated_residual_error(u, nu, residuals='both', broken_baselines_weight =
     for i in range(len(u)):
         model_covariance = sky_covariance(u[i], 0, nu, S_low=model_limit, S_high=10, tile_diameter=tile_diameter)
         sky_residuals = sky_covariance(u[i], 0, nu, S_low=1e-3, S_high=model_limit, tile_diameter=tile_diameter)
-        scale = numpy.diag(numpy.zeros_like(nu)) + 1
+        scale = np.diag(np.zeros_like(nu)) + 1
 
         if weights is None:
             nu_cov = 2*gain_averaged_covariance*model_covariance + \
